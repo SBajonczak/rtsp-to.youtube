680190430613848fadcf95c1733e9d84ff0c8d85
sascha bajonczak <sba@realcore.de>
2023-05-21 18:57:10 +0200

    Finalized downloader script and added partial readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# About
+This little tool will help you to stream your local camera rtsp stream to youtube. 
+
+# Structure
+This "project" conains two container definitions
+1. Publish 
+2. download
+
+The follwoing section will describe, how to build it
+# How to build
+
+## Publish
+This container will forward your livestream. 
+
+You can build your container in the publish directory with 
+```
+docker built -t yourcontainername .
+```
+
+## download
+
+This container will download periodicly 
+
+# Usage
--- download/entrypoint.sh
+++ download/entrypoint.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
-if [ "$#" -ne 1 ]; then
-  >&2 echo "Required arguments: YOUTUBE_STREAM_URL"
+if [ "$#" -ne 3 ]; then
+  >&2 echo "Required arguments: YOUTUBE_STREAM_URL DURATION_IN_SECONDS DURATION_IN_SECONDS_TO_WAIT"
   exit 1
 fi
 
-if [ ! -d /data ]; then
-  >&2 echo "Expected Docker mounted volume at /data"
-  exit 1
-fi
-cd /data
-
+# -f 93 is for 360p stream
 YOUTUBE_STREAM_URL=$1
+DURATION_IN_SECONDS=$2
+DURATION_IN_SECONDS_TO_WAIT=$3
 echo "YOUTUBE_STREAM_URL=$YOUTUBE_STREAM_URL"
+echo "DURATION_IN_SECONDS=$DURATION_IN_SECONDS"
+echo "DURATION_IN_SECONDS_TO_WAIT=$DURATION_IN_SECONDS_TO_WAIT"
 
 while :
 do
   echo "Downloading video for 5 seconds"
-  # option -f 93 is for 360p stream
   URL=$(youtube-dl -g -f 93 $YOUTUBE_STREAM_URL)
   echo "Stream internal address(es): $URL"
-  echo "$URL" | xargs -n 1 -I {} ffmpeg -t 5 -i "{}" -f null -
-  echo "Waiting for 10 minutes"
-  sleep 600
+  echo "$URL" | xargs -n 1 -I {} ffmpeg -t $DURATION_IN_SECONDS -i "{}" -f null -
+  echo "Waiting for $DURATION_IN_SECONDS_TO_WAIT seconds"
+  sleep $DURATION_IN_SECONDS_TO_WAIT
+done
